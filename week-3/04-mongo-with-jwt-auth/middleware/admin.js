165c8eee5a382c922dd41d47b46361d537407ce3
sanopopular <prabeshbhandari842@gmail.com>
2024-01-09 14:13:30 +0545

    the first forked commit is this

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -2,6 +2,7 @@
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const a = 2
 }
 
 module.exports = adminMiddleware;
